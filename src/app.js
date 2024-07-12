c73cd6efeaeb21c461d519e36d0744c0e6c124f4
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-07-12 18:08:11 +0200

    fixes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,68 @@
 import * as React from 'react'
-import '../public/globals.css'
+import './globals.css'
 import { FlexCol } from './utils'
+import * as Lucide from 'lucide-react'
 
 export var App = () => {
-  return <div>Hey there.</div>
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: '#FFFFFF',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        gap: 40,
+      }}
+    >
+      <img
+        data-aspect-ratio-locked
+        src='./assets/me-ia-small.png'
+        style={{
+          width: 100,
+          height: 100,
+          contain: 'layout',
+          borderRadius: 50,
+          overflow: 'hidden',
+        }}
+      />
+      <span
+        style={{
+          wordBreak: 'break-word',
+          contain: 'layout',
+          width: 'max-content',
+          fontWeight: 400,
+          fontFamily: 'Lato',
+          fontStyle: 'normal',
+          fontSize: '3em',
+          height: 'max-content',
+        }}
+      >
+        Federico Ruggi
+      </span>
+      <div
+        style={{
+          contain: 'layout',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 20,
+          height: 'max-content',
+          width: 'max-content',
+        }}
+      >
+        <a href='https://github.com/ruggi'>
+          <Lucide.GithubIcon />
+        </a>
+        <a href='https://hachyderm.io/@federico'>
+          <Lucide.EarthIcon />
+        </a>
+        <a href='https://linkedin.com/in/federicoruggi'>
+          <Lucide.LinkedinIcon />
+        </a>
+      </div>
+    </div>
+  )
 }
--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -1,69 +1,10 @@
 import * as React from 'react'
 import { Scene, Storyboard } from 'utopia-api'
 import { App } from '/src/app.js'
-import { Playground } from '/src/playground.js'
 import * as Lucide from 'lucide-react'
 
 export var storyboard = (
   <Storyboard>
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-        backgroundColor: '#FFFFFF',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        gap: 40,
-      }}
-    >
-      <img
-        data-aspect-ratio-locked
-        src='./assets/me-ia-small.png'
-        style={{
-          width: 100,
-          height: 100,
-          contain: 'layout',
-          borderRadius: 50,
-          overflow: 'hidden',
-        }}
-      />
-      <span
-        style={{
-          wordBreak: 'break-word',
-          contain: 'layout',
-          width: 'max-content',
-          fontWeight: 400,
-          fontFamily: 'Lato',
-          fontStyle: 'normal',
-          fontSize: '3em',
-          height: 'max-content',
-        }}
-      >
-        Federico Ruggi
-      </span>
-      <div
-        style={{
-          contain: 'layout',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          gap: 20,
-          height: 'max-content',
-          width: 'max-content',
-        }}
-      >
-        <a href='https://github.com/ruggi'>
-          <Lucide.GithubIcon />
-        </a>
-        <a href='https://hachyderm.io/@federico'>
-          <Lucide.EarthIcon />
-        </a>
-        <a href='https://linkedin.com/in/federicoruggi'>
-          <Lucide.LinkedinIcon />
-        </a>
-      </div>
-    </div>
+    <App />
   </Storyboard>
 )
